2e77cfe2faadc468a8f6087f520130d5899de565
Brendan P <64713032+brendisurfs@users.noreply.github.com>
2022-09-16 12:25:53 -0700

    added notes about shared state and updated cmd processor
    Signed-off-by: Brendan P <64713032+brendisurfs@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,15 @@
 //  5. Tasks spawned by tokio::spawn must implement Send.
 
 // ------------ project -----------------
+use bytes::Bytes;
+use log::{error, info, warn};
 use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
+
+// simpler to read + easier for me to remember later.
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
@@ -16,6 +23,10 @@ async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379")
         .await
         .expect("could not bind to port");
+
+    info!("starting server");
+
+    let db: Db = Arc::new(Mutex::new(HashMap::new()));
 
     loop {
         let (socket, _) = listener
@@ -26,19 +37,20 @@ async fn main() {
         // 1. a Tokio task is an async green thread.
         //  - Created by passing an async block to tokio::spawn.
         //  - Returns a join handle, so that the caller can interact with that spawned task.
+        info!("accepted incoming connection");
+        let db = db.clone();
         tokio::spawn(async move {
             // a new task is spawned for each inbound socket.
             // The socket is moved to the new task and processed there.
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+// takes in the socket and the *shared handle* to the hash map as an arg.
+async fn process(socket: TcpStream, db: Db) {
+    // THIS FEELS GOOD LIKE IM WRITING SOME REASON OR SOMETHING LOCAL USES.
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    let mut db = HashMap::new();
 
     // the `Connection` lets us read/write redis **frames** instead of byte streams.
     // the Connection type is defined by mini-redis.
@@ -48,12 +60,18 @@ async fn process(socket: TcpStream) {
         let cmd_from_frame = Command::from_frame(frame).expect("could not read from frame");
         let response = match cmd_from_frame {
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                // NOTE: this is important for mutating the shared data.
+                let mut db = db.lock().expect("could not lock arc db");
+                let cmd_key = cmd.key().to_string();
+                let cmd_value = cmd.value().clone();
+
+                db.insert(cmd_key, cmd_value);
+
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().expect("could not lock arc db");
                 if let Some(value) = db.get(cmd.key()) {
-                    // Frame::Bulk will expect data to be type "Bytes".
                     Frame::Bulk(value.clone().into())
                 } else {
                     Frame::Null
@@ -67,4 +85,15 @@ async fn process(socket: TcpStream) {
             .await
             .expect("could not write frame");
     }
+}
+
+// since we have little contention with synchronous mutex, mutex sharding is fine to use.
+type ShardedDb = Arc<Vec<Mutex<HashMap<String, Vec<u8>>>>>;
+
+fn new_sharded_db(num_shards: usize) -> ShardedDb {
+    let mut db = Vec::with_capacity(num_shards);
+    for _ in 0..num_shards {
+        db.push(Mutex::new(HashMap::new()));
+    }
+    Arc::new(db)
 }
